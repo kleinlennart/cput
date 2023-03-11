15e250b8f257ec073a64189579fe6ce957c28fe8
Lennart Klein <31488915+kleinlennart@users.noreply.github.com>
2023-03-11 17:46:45 +0100

    change pipe

--- R/pasteAsRecode.R
+++ R/pasteAsRecode.R
@@ -6,6 +6,6 @@
 pasteAsRecode <- function() {
   clipy <- clipr::read_clip_tbl(header = FALSE)
   glue <- stringr::str_glue('"{clipy$V1}" = "{clipy$V2}"')
-  recoder <- paste0("var %>% recode(\n  ", paste0(glue, collapse = ",\n  "), "\n)")
+  recoder <- paste0("recode(\n  ", paste0(glue, collapse = ",\n  "), "\n)")
   rstudioapi::insertText(recoder)
 }
