c3093850c323a3cbee82c8228b410289a1132236
Lennart Klein <31488915+kleinlennart@users.noreply.github.com>
2023-05-08 18:30:03 +0200

    add fct_recode

--- R/pasteAsRecode.R
+++ R/pasteAsRecode.R
@@ -9,3 +9,16 @@ pasteAsRecode <- function() {
   recoder <- paste0("recode(\n  ", paste0(glue, collapse = ",\n  "), "\n)")
   rstudioapi::insertText(recoder)
 }
+
+
+#' Paste Clipboard as fct_recode
+#'
+#' Call this function as an Addin to insert a recoder based on codebook clipboard
+#'
+#' @export
+pasteAsFctRecode <- function() {
+  clipy <- clipr::read_clip_tbl(header = FALSE)
+  glue <- stringr::str_glue('"{clipy$V2}" = "{clipy$V1}"')
+  recoder <- paste0("fct_recode(\n  ", paste0(glue, collapse = ",\n  "), "\n)")
+  rstudioapi::insertText(recoder)
+}
