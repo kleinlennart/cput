9704610d22d39c8137da6e7479b5da7daa4662de
Lennart Klein <31488915+kleinlennart@users.noreply.github.com>
2023-06-11 13:54:38 +0200

    add here translator

---
+++ R/here.R
@@ -0,0 +1,24 @@
+#' Translate path to here::here path
+#'
+#' Call this function as an addin to insert here path from selection.
+#'
+#' @export
+translateHere <- function() {
+  context <- rstudioapi::getActiveDocumentContext()
+  selection <- rstudioapi::primary_selection(context)
+  path <- selection$text
+  # selection$range ## use to insert?
+
+  if (!stringr::str_detect(path, '^\\"') | !stringr::str_detect(path, '\\"$')) {
+    usethis::ui_stop("Selection included more than just the path.")
+  }
+
+  usethis::ui_info(path)
+  here <- stringr::str_split(path, "/") %>%
+    unlist() %>%
+    paste(collapse = '", "') %>%
+    paste0('here::here(', ., ')')
+
+  rstudioapi::insertText(here)
+  usethis::ui_done("Inserted!")
+}
